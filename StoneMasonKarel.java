15d45e162958af78f1336aa2299ea8e50782fece
CS106A <>
2018-06-17 12:08:22 -0400

    {"types":"C","runs":11}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -1,11 +1,8 @@
 /*
  * File: StoneMasonKarel.java
  * --------------------------
- * The StoneMasonKarel subclass as it appears here does nothing.
- * When you finish writing it, it should solve the "repair the quad"
- * problem from Assignment 1.  In addition to editing the program,
- * you should be sure to edit this comment so that it no longer
- * indicates that the program does nothing.
+ * The StoneMasonKarel subclass "fixes" broken columns in a world.  
+ * Karel always starts in Row 1, Avenue 1.  Ave
  */
 
 import stanford.karel.*;

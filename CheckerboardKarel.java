b2c7dfc9ba1a73ae6dcd11ffb01121ab8042c1b7
CS106A <>
2018-06-17 18:34:44 -0400

    {"types":"C","runs":1}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -1,10 +1,8 @@
 /*
  * File: CheckerboardKarel.java
  * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
- * worlds supplied in the starter folder.
+ * CheckerboardKarel has one universal solution (allWorldSolution()) and
+ * two edge cases, singleAvenue.
  */
 
 import stanford.karel.*;

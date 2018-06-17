d526c4390c8bc7935cd993bbc65d9967f994152c
CS106A <>
2018-06-17 14:49:28 -0400

    {"types":"C","runs":11}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -12,7 +12,7 @@ import stanford.karel.*;
 public class CheckerboardKarel extends SuperKarel {
 
 public void run() {
-	while (checkerboardUnfinished() ) {
+	while (() ) {
 		addCheckerboard();
 		gotoNextRow();
 	}
